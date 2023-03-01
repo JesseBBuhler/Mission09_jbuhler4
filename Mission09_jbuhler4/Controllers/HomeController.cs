a1ff50997ef8f1f0b69d8c03cfe4dd928a68e617
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-02-28 17:50:13 -0700

    Setting up app structure and repository

---
+++ Mission09_jbuhler4/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_jbuhler4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jbuhler4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index()
+        {
+            var books = repo.Books.ToList();
+            return View(books);
+        }
+    }
+}
